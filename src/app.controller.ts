0f8ab72379eab731e81f4e330e7ebbb50ffddb88
Dernorayytor <thenet21018@gmail.com>
2024-08-06 11:00:52 +0700

    เพิ่มคำอธิบาย controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,3 +10,5 @@ export class AppController {
     return this.appService.getHello();
   }
 }
+
+//คอนโทรลเลอคือคนที่รับรีเควสมา Controller() ต้องระบุพาทเริ่มต้นซึ่งถ้าไม่ระบุก็แปลว่าใช้แสลดของมันเอง โดยเก็ทที่เราสร้างเนี่ย มันจะรีเทริน appService ขึ้นมาจากการInjectable AppService จาก ./app.service
