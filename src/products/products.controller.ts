70b17cf66934b81c96217108f62c4eb3311ace41
Dernorayytor <thenet21018@gmail.com>
2024-08-06 12:16:48 +0700

    test @Get()

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    @Get()
+    getAllProducts(){
+        return 'This action returns all products.';
+    }
+}
