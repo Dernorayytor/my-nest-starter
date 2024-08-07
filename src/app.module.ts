9fdeaa333e8b37931dc3df90d4778defccb23634
Dernorayytor <thenet21018@gmail.com>
2024-08-07 10:18:55 +0700

    หลังจาก nest g resource user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://root:example@localhost:27017/dernorayytor?authSource=admin')],
+  imports: [MongooseModule.forRoot('mongodb://root:example@localhost:27017/dernorayytor?authSource=admin'), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
