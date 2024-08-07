a51583c2c28e4a4db3d5fc91273b426392076bf9
Dernorayytor <thenet21018@gmail.com>
2024-08-07 10:59:17 +0700

    UpdateUserDto readonly

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+    readonly username?: string;
+    readonly age?: number;
+    readonly email?: string;
+    readonly password?: string;
+}
