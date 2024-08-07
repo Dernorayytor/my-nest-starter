271de4b097a7227c142d93997d7ee9dfd00d8a3d
Dernorayytor <thenet21018@gmail.com>
2024-08-07 10:53:34 +0700

    CreateUserDto readonly

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,6 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    readonly username: string;
+    readonly age?: number;
+    readonly email: string;
+    readonly password: string;
+}
