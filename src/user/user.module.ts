ea62646b200f60da945f8234f945528ede5e3f73
Dernorayytor <thenet21018@gmail.com>
2024-08-07 11:20:32 +0700

    import MongooseModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   controllers: [UserController],
   providers: [UserService],
