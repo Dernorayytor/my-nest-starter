4962b4fb8daa8ac3bcfebbf78cee31f0e39e8921
Dernorayytor <thenet21018@gmail.com>
2024-08-07 11:42:43 +0700

    service คุยกับ schema ติดต่อกับ mongoose เพื่อ มองโก

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './schemas/user.schemas';
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
