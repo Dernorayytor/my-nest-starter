3ac369fd0158df0cbe9d775dd639b4a90ad5d2a3
Dernorayytor <thenet21018@gmail.com>
2024-08-14 10:43:23 +0700

    Update user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable, Delete , NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { User, UserDocument } from './schemas/user.schemas';
 import { error } from 'console';
 
@@ -16,28 +15,10 @@ export class UserService {
     return result.save();
   }
 
-  async findAll(): Promise<User[]> {
-    return this.userModel.find().exec();
-  }
-
   async findOne(id: string): Promise<User> {
     return this.userModel.findById(id).exec();
   }
-
-  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
-    const result = this.userModel.findByIdAndUpdate(
-      id,updateUserDto,{new: true}
-    ).exec();
-    return result;
-  }
-
-  async remove(id: string) {
-      const result = await this.userModel.findByIdAndDelete(id).exec();
-      if (!result) {
-        throw new NotFoundException('id not found');
-      }
-      return { message: 'User deleted successfully' };
-  }//ย่อโค้ดไม่ต้องใช้ try catch ก็ได้
+//ย่อโค้ดไม่ต้องใช้ try catch ก็ได้
 }
 //new: true ใส่เพื่อบ่งบอกว่าเอาค่าใหม่หลังอัพเดทให้ด้วย
 //exec() เป็นอะไรที่ต้องใส่ async และ Promise<xxx>
