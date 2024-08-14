9bd3f9903c8540cf21a52d09f0b505a464f913d3
Dernorayytor <thenet21018@gmail.com>
2024-08-14 10:09:27 +0700

    user.controller.ts ใช้งานแค่ post get

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,23 +12,9 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
-  }
-
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(id);
   }
-
-  @Patch(':id') //แล้วแต่หลายที่บางทีก็ใช้ put หรือ patch
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(id);
-  }
 }
+//ใช้งานแค่ post get
