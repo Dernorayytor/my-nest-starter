391b0ed2814bf10c9caa25dd4facb46fa7bef29b
Dernorayytor <thenet21018@gmail.com>
2024-08-14 09:56:51 +0700

    add Orders

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://root:example@localhost:27017/dernorayytor?authSource=admin'), UserModule],
+  imports: [MongooseModule.forRoot('mongodb://root:example@localhost:27017/dernorayytor?authSource=admin'), UserModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
