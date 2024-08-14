d444294e01c08fdea0f4aa49d5fba085c8285e2c
Dernorayytor <thenet21018@gmail.com>
2024-08-14 09:37:59 +0700

    setting Types.ObjectId ต้องเป็นของ user นั้นๆเท่านั้นถึงจะรีจิสเข้าไป จะ Reference to User model

--- src/user/schemas/user.schemas.ts
+++ src/user/schemas/user.schemas.ts
@@ -1,12 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export type UserDocument = User & Document;
 
 @Schema()
 export class User {
-  @Prop()
-  name: string;
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  name: Types.ObjectId;
 
   @Prop()
   age: number;
